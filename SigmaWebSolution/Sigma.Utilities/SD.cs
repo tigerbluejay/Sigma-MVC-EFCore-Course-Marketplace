2c9ec3b3abd6662c0cdae7c13b98cbb7ef1fa3be
tigerbluejay <josemariairiarte@gmail.com>
2023-06-26 14:21:46 -0300

    Removed Employee from DB, Utilities Project and Register related Models

--- SigmaWebSolution/Sigma.Utilities/SD.cs
+++ SigmaWebSolution/Sigma.Utilities/SD.cs
@@ -10,7 +10,6 @@ namespace Sigma.Utilities
     {
         public const string Role_User_Indi = "Individual";
         public const string Role_Admin = "Admin";
-        public const string Role_Employee = "Employee";
 
 		public const string StatusPending = "Pending";
 		public const string StatusApproved = "Approved";
--- SigmaWebSolution/SigmaWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SigmaWebSolution/SigmaWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -128,7 +128,6 @@ namespace SigmaWeb.Areas.Identity.Pages.Account
             if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
             {
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
-				_roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
 				_roleManager.CreateAsync(new IdentityRole(SD.Role_User_Indi)).GetAwaiter().GetResult();
 
 			}
